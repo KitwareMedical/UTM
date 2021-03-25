479b78a5338e98a79f05777bd6eb10d0b82cffae
Samuel Gerber <samuel.gerber@kitware.com>
2021-03-25 17:06:40 -0400

    ADD: oasis-1 scales script

--- python/run-oasis-1-scales.py
+++ python/run-oasis-1-scales.py
@@ -29,7 +29,7 @@ scales = {
 
 for f in scales.keys():
   try:
-    os.mkdir("{0}/{2}".format(output_folder, f)
+    os.mkdir("{0}/{2}".format(output_folder, f))
   except FileExistsError:
     pass
 
