670f8689e9ca6bfcba053607fb1ea20a8f9eaef8
Samuel Gerber <samuel.gerber@kitware.com>
2021-03-25 17:03:18 -0400

    ADD: oasis-1 scales script

---
+++ python/run-oasis-1-scales.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+import os
+
+for key, value in scales.items():
+    os.system(("Rscript ../Scripts/run.utm.barycenter.R {3}/gray {1}"
+                 " --working.folder {2}/results/{3}"
+                 " --config {0}" ).format(value, csv_out_file, output_folder, key ) )
+
+##Run script
+parser = argparse.ArgumentParser(description=('Perform On OASIS-1 data.'
+        ' See download-oasis-1.py for obtaining data.'))
+parser.add_argument('--input_folder', required=True,
+        help='path to folder containg oasis-1 data')
+parser.add_argument('--output_folder', required=True,
+        help='output path to store files to')
+args = parser.parse_args()
+
+input_folder = args.input_folder
+output_folder = args.output_folder
+
+
+#Folders for different preprocessing options and intermediate images
+preprocess_folder = "{0}/gray".format(output_folder)
+
+scales = {
+ "gray_scale_1" : "oasis-1-scale-1.yaml",
+ "gray_scale_2" : "oasis-1-scale-2.yaml",
+ "gray_scale_3" : "oasis-1-scale-3.yaml"
+}
+
+for f in scales.keys():
+  try:
+    os.mkdir("{0}/{2}".format(output_folder, )
+  except FileExistsError:
+    pass
+
+#Modified input csv file to match R script expectations
+csv_out_file = "{0}/data.csv".format(output_folder)
+
+if __name__ == '__main__':
+  main()
